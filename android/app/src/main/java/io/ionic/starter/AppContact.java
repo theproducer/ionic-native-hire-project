f7a3280350d18f07d0f8639768b419a3ec2242da
Joseph Pender <joey@prodcode.net>
2020-10-31 15:12:09 -0500

    [android] added ability to query contacts by search term and id

--- android/app/src/main/java/io/ionic/starter/AppContact.java
+++ android/app/src/main/java/io/ionic/starter/AppContact.java
@@ -3,16 +3,34 @@ package io.ionic.starter;
 import java.util.ArrayList;
 
 public class AppContact {
+    String id;
     String firstName;
     String lastName;
     ArrayList<String> emailAddresses;
     ArrayList<String> phoneNumbers;
 
     public AppContact() {
+        this.id = "";
         this.firstName = "";
         this.lastName = "";
         this.emailAddresses = new ArrayList<String>();
         this.phoneNumbers = new ArrayList<String>();
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName == null) {
+            this.firstName = "";
+        } else {
+            this.firstName = firstName;
+        }
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null) {
+            this.lastName = "";
+        } else {
+            this.lastName = lastName;
+        }
     }
 
     public void addEmailAddress(String email) {
--- android/app/src/main/java/io/ionic/starter/Contacts.java
+++ android/app/src/main/java/io/ionic/starter/Contacts.java
@@ -5,7 +5,6 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.content.pm.PackageManager;
-import android.util.Log;
 
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
@@ -14,15 +13,17 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @NativePlugin(
-    requestCodes = { Contacts.GET_ALL_REQUEST }
+    requestCodes = { Contacts.GET_ALL_REQUEST, Contacts.GET_BY_ID_REQUEST, Contacts.GET_ALL_BY_SEARCH_REQUEST }
 )
 public class Contacts extends Plugin {
   static final int GET_ALL_REQUEST = 30033;
+  static final int GET_BY_ID_REQUEST = 30034;
+  static final int GET_ALL_BY_SEARCH_REQUEST = 30035;
 
   @PluginMethod()
   public void getAll(PluginCall call) {
@@ -33,20 +34,109 @@ public class Contacts extends Plugin {
     }
 
     JSObject result = new JSObject();
-    JSArray contacts = this.getAllContacts();
+    JSArray contacts = this.getAllContacts(null);
     result.put("contacts", contacts);
-    call.success(result);
+    call.resolve(result);
   }
 
-  protected JSArray getAllContacts() {
-    LinkedHashMap<Integer, AppContact> contacts = new LinkedHashMap<Integer, AppContact>();
+  @PluginMethod()
+  public void getBySearch(PluginCall call) {
+    if (!hasPermission(Manifest.permission.READ_CONTACTS) || !hasPermission(Manifest.permission.WRITE_CONTACTS)) {
+      saveCall(call);
+      pluginRequestPermissions(new String[] { Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS }, GET_ALL_BY_SEARCH_REQUEST);
+      return;
+    }
 
+    String searchTerm = call.getString("searchTerm");
+    if (searchTerm == null || searchTerm.isEmpty()) {
+      call.reject("a search term is required");
+      return;
+    }
+
+    JSObject result = new JSObject();
+    JSArray contacts = this.getAllContacts(searchTerm);
+    result.put("contacts", contacts);
+    call.resolve(result);
+  }
+
+  @PluginMethod()
+  public void getById(PluginCall call) {
+    if (!hasPermission(Manifest.permission.READ_CONTACTS) || !hasPermission(Manifest.permission.WRITE_CONTACTS)) {
+      saveCall(call);
+      pluginRequestPermissions(new String[] { Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS }, GET_BY_ID_REQUEST);
+      return;
+    }
+
+    String contactId = call.getString("contactId");
+    if (contactId == null || contactId.isEmpty()) {
+      call.reject("a contact id is required");
+      return;
+    }
+
+    JSObject contact = this.getContactById(contactId);
+    JSObject result = new JSObject();
+    result.put("contact", contact);
+    call.resolve(result);
+  }
+
+  protected JSObject getContactById(String id) {
     final String[] PROJECTION = new String[]{
-            ContactsContract.Data.CONTACT_ID,
-            ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME,
-            ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME,
-            ContactsContract.Data.DATA1,
-            ContactsContract.Data.MIMETYPE
+      ContactsContract.Data.CONTACT_ID,
+      ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME,
+      ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME,
+      ContactsContract.CommonDataKinds.GroupMembership.DISPLAY_NAME,
+      ContactsContract.Data.DATA1,
+      ContactsContract.Data.MIMETYPE
+    };
+
+    ContentResolver resolver = this.getContext().getContentResolver();
+
+    Cursor cursor = resolver.query(
+            ContactsContract.Data.CONTENT_URI,
+            PROJECTION,
+            ContactsContract.Data.CONTACT_ID + " = ?" +
+                    " AND (" +
+                    ContactsContract.Data.MIMETYPE + " = ?" +
+                    " OR " +
+                    ContactsContract.Data.MIMETYPE + " = ?" +
+                    " OR " +
+                    ContactsContract.Data.MIMETYPE + " = ?" +
+                    ")"
+            ,
+            new String[]{
+                    id,
+                    ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE,
+                    ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE,
+            },
+            null
+    );
+
+    ArrayList<AppContact> allContacts = this.processContactsCursor(cursor, null);
+    if (allContacts.get(0) != null) {
+      AppContact contact = allContacts.get(0);
+
+      JSObject jsonContact = new JSObject();
+      jsonContact.put("id", contact.id);
+      jsonContact.put("firstName", contact.firstName);
+      jsonContact.put("lastName",  contact.lastName);
+      jsonContact.put("phoneNumbers", new JSArray(contact.phoneNumbers));
+      jsonContact.put("emailAddresses", new JSArray(contact.emailAddresses));
+
+      return jsonContact;
+    }
+
+    return null;
+  }
+
+  protected JSArray getAllContacts(String searchTerm) {
+    final String[] PROJECTION = new String[]{
+      ContactsContract.Data.CONTACT_ID,
+      ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME,
+      ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME,
+      ContactsContract.CommonDataKinds.GroupMembership.DISPLAY_NAME,
+      ContactsContract.Data.DATA1,
+      ContactsContract.Data.MIMETYPE
     };
 
     ContentResolver resolver = this.getContext().getContentResolver();
@@ -68,6 +158,25 @@ public class Contacts extends Plugin {
             null
     );
 
+    ArrayList<AppContact> contactList = this.processContactsCursor(cursor, searchTerm);
+
+    JSArray jsonContacts = new JSArray();
+
+    for (AppContact contact : contactList) {
+      JSObject jsonContact = new JSObject();
+      jsonContact.put("id", contact.id);
+      jsonContact.put("firstName", contact.firstName);
+      jsonContact.put("lastName",  contact.lastName);
+      jsonContact.put("phoneNumbers", new JSArray(contact.phoneNumbers));
+      jsonContact.put("emailAddresses", new JSArray(contact.emailAddresses));
+      jsonContacts.put(jsonContact);
+    }
+
+    return jsonContacts;
+  }
+
+  private ArrayList<AppContact> processContactsCursor(Cursor cursor, String searchTerm) {
+    LinkedHashMap<Integer, AppContact> contacts = new LinkedHashMap<Integer, AppContact>();
     try {
       final int idPos = cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID);
       final int firstNamePos = cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
@@ -84,6 +193,7 @@ public class Contacts extends Plugin {
 
         if (contacts.get(contactId) == null) {
           AppContact newContact = new AppContact();
+          newContact.id = Integer.toString(contactId);
           switch (mimeType) {
             case ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE:
               newContact.addEmailAddress(emailOrPhone);
@@ -92,8 +202,8 @@ public class Contacts extends Plugin {
               newContact.addPhoneNumber(emailOrPhone);
               break;
             case ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE:
-              newContact.firstName = firstName;
-              newContact.lastName = lastName;
+              newContact.setFirstName(firstName);
+              newContact.setLastName(lastName);
               break;
           }
           contacts.put(contactId, newContact);
@@ -108,8 +218,8 @@ public class Contacts extends Plugin {
                 updatedContact.addPhoneNumber(emailOrPhone);
                 break;
               case ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE:
-                updatedContact.firstName = firstName;
-                updatedContact.lastName = lastName;
+                updatedContact.setFirstName(firstName);
+                updatedContact.setLastName(lastName);
                 break;
             }
           }
@@ -119,18 +229,39 @@ public class Contacts extends Plugin {
       cursor.close();
     }
 
-
-    JSArray jsonContacts = new JSArray();
+    ArrayList<AppContact> contactsList = new ArrayList<>();
     for (Map.Entry<Integer, AppContact> contact : contacts.entrySet()) {
-      JSObject jsonContact = new JSObject();
-      jsonContact.put("firstName", contact.getValue().firstName);
-      jsonContact.put("lastName",  contact.getValue().lastName);
-      jsonContact.put("phoneNumbers", new JSArray(contact.getValue().phoneNumbers));
-      jsonContact.put("emailAddresses", new JSArray(contact.getValue().emailAddresses));
-      jsonContacts.put(jsonContact);
+      if (searchTerm != null) {
+        boolean found = false;
+        if (contact.getValue().firstName.contains(searchTerm) || contact.getValue().lastName.contains(searchTerm)) {
+          found = true;
+        }
+
+        if (!found) {
+          for (String email : contact.getValue().emailAddresses) {
+            if (email.contains(searchTerm)) {
+              found = true;
+              break;
+            }
+          }
+
+          for (String phone : contact.getValue().phoneNumbers) {
+            if (phone.contains(searchTerm)) {
+              found = true;
+              break;
+            }
+          }
+        }
+
+        if (found) {
+          contactsList.add(contact.getValue());
+        }
+      } else {
+        contactsList.add(contact.getValue());
+      }
     }
 
-    return jsonContacts;
+    return contactsList;
   }
 
   @Override
@@ -144,13 +275,21 @@ public class Contacts extends Plugin {
 
     for(int result : grantResults) {
       if (result == PackageManager.PERMISSION_DENIED) {
-        savedCall.error("User denied permission");
+        savedCall.reject("User denied permissions");
         return;
       }
     }
 
     if (requestCode == GET_ALL_REQUEST) {
       this.getAll(savedCall);
+    }
+
+    if (requestCode == GET_BY_ID_REQUEST) {
+      this.getById(savedCall);
+    }
+
+    if (requestCode == GET_ALL_BY_SEARCH_REQUEST) {
+      this.getBySearch(savedCall);
     }
   }
 }
